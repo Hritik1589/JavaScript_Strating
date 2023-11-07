f97f14d92f30b1b9bde3041990d4bbe9b92eaf79
Hritik Negi <73572419+Hritik1589@users.noreply.github.com>
2023-11-07 05:17:54 +0000

    pushed

---
+++ JavaScript_Beginners/Data_Types.js
@@ -0,0 +1,33 @@
+/*
+There are Two types of data Types in the Java Script:
+1 - Primitive Data Type - Which can only store single values; Ex - String,Boolean,Undefined,Number,BigInt,null
+2 - Non-Primitive Data Type - Which can store multiples values under themselves; Ex - Object,Array
+
+*/
+//String
+let a = "Hritik";
+console.log(typeof a);// type String whatever comes under double quotes is type of string
+
+//Number
+let b = 12345
+console.log(typeof b);// number
+
+//boolean
+let c = true
+console.log(typeof c);//boolean
+
+//undefined
+let d;
+console.log(typeof d);//undefined as the value does not defined
+
+//null
+let e = null
+console.log(typeof e);//type is object,Null plays key role in the Api calls
+
+//array
+let f = [1,2,3,4];
+console.log(typeof f);
+
+//object
+let g = {name:"Hritik",id:1}
+console.log(typeof g);
